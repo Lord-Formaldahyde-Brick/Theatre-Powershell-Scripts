32362d0dcb63688ed41946ec1d282b71cef559be
Colin Evans <cybclinic@gmail.com>
2023-04-01 08:50:32 +0100

    Clean up code  and Alias nms

--- New-MultiShow.ps1
+++ New-MultiShow.ps1
@@ -1,10 +1,23 @@
 function New-MultiShow {
     [CmdletBinding(PositionalBinding=$True)]
-    [alias("ms")]
+    [alias("nms")]
     param (
         [Parameter(Mandatory = $True)]           
         [string]$showName
     )
+    function makeSubFolders () {
+        [int]$noOfPlays = Read-Host "How many Plays?"
+        for ($i -eq 0; $i -lt $noOfPlays; $i++) {
+            $j = $i+1
+            $nameOfPlay = Read-Host "Name of Play"$j"?"
+            New-Item -Name $nameOfPlay -ItemType Directory
+            Set-Location $nameOfPlay
+            New-Item -Name Audio -ItemType Directory
+            New-Item -Name Images -ItemType Directory
+            New-Item -Name Video -ItemType Directory
+            Set-Location ..
+        }
+    }
     $d = Get-Date
     $justDate = $d.tostring("dd-MM-yyyy")
     [string]$fullName = "$showName"+"_"+"$justDate"
@@ -18,32 +31,12 @@ function New-MultiShow {
         }
         New-Item -Name $extendName -ItemType Directory
         Set-Location $extendName
-        [int]$noOfPlays = Read-Host "How many Plays?"
-        for ($i -eq 0; $i -lt $noOfPlays; $i++) {
-            $j = $i+1
-            $nameOfPlay = Read-Host "Name of Play"$j"?"
-            New-Item -Name $nameOfPlay -ItemType Directory
-            Set-Location $nameOfPlay
-            New-Item -Name Audio -ItemType Directory
-            New-Item -Name Images -ItemType Directory
-            New-Item -Name Video -ItemType Directory
-            Set-Location ..
-            }
+        makeSubFolders
     }
     Else {
         New-Item -Name $fullName -ItemType Directory
         Set-Location $fullName
-        [int]$noOfPlays = Read-Host "How many Plays?"
-        for ($i -eq 0; $i -lt $noOfPlays; $i++) {
-            $j = $i+1
-            $nameOfPlay = Read-Host "Name of Play"$j"?"
-            New-Item -Name $nameOfPlay -ItemType Directory
-            Set-Location $nameOfPlay
-            New-Item -Name Audio -ItemType Directory
-            New-Item -Name Images -ItemType Directory
-            New-Item -Name Video -ItemType Directory
-            Set-Location ..
-        }            
+        makeSubFolders     
     }
     Set-Location ..
 }
