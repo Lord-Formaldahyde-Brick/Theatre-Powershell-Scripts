81fbd6a153136275df9642b5b09a98aafa17a81a
Colin Evans <cybclinic@gmail.com>
2023-04-01 12:38:57 +0100

    Outline

--- New-ExifFileManager.ps1
+++ New-ExifFileManager.ps1
@@ -1,7 +1,14 @@
 
 # Building at the mo, will look odd for a bit, this is just a placeholder for bits of code
+# *****************************************************************************
 
-# Extract Creation Date - Get-ChildItem will be replaced by a UI file selection
+
+# Start by presenting UI File Selection Box in Multi-Select mode.
+
+# Once Ok is pressed present a dropdown with Lecturer name to provide the 
+# head folder path for that person.
+
+# Extract Creation Date - Get-ChildItem will be replaced by the UI file selection
 foreach($file in Get-ChildItem -Name){
     $jn = exiftool -j -q $file 
     $jn = $jn | ConvertFrom-Json
@@ -9,3 +16,6 @@ foreach($file in Get-ChildItem -Name){
     $dt.Substring(0,10).Replace(":","-")
 }
 
+# Append exif createdate to Lecturer name and create a subfolder using this concat string 
+# to store the current file if subfolder doesn't exist,
+#  if it does simply store file there.
