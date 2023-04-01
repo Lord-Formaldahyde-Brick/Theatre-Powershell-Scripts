04a11f3b6d96699cab9decaf9171bbaad411f6f2
Colin Evans <cybclinic@gmail.com>
2023-04-01 09:45:55 +0100

    ForEach in place

--- New-ExifFileManager.ps1
+++ New-ExifFileManager.ps1
@@ -1,11 +1,11 @@
 
 # Building at the mo, will look odd for a bit, this is just a placeholder for bits of code
 
-# Extract Creation Date - needs foreach
-$jn = exiftool -j -q * 
-$jn = $jn | ConvertFrom-Json
-#$jn > data.json
-[string]$dt = $jn.createdate #| sed 's/ .*//' | sed 's/:/-/g'
-$dt.Substring(0,10).Replace(":","-")
-
+# Extract Creation Date - Get-ChildItem will be replaced by a UI file selection
+foreach($file in Get-ChildItem -Name){
+    $jn = exiftool -j -q $file 
+    $jn = $jn | ConvertFrom-Json
+    [string]$dt = $jn.createdate
+    $dt.Substring(0,10).Replace(":","-")
+}
 
