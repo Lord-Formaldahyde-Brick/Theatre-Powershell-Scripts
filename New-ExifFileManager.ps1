f7e8bfb8afe20c77bc424086948fb16f52cb1c9c
Colin Evans <cybclinic@gmail.com>
2023-04-01 09:30:26 +0100

    Ignore, just a play-pen at the mo

--- New-ExifFileManager.ps1
+++ New-ExifFileManager.ps1
@@ -1,4 +1,6 @@
 
+# Building at the mo, will look odd for a bit, this is just a placeholder for bits of code
+
 # Extract Creation Date - needs foreach
 $jn = exiftool -j -q * 
 $jn = $jn | ConvertFrom-Json
