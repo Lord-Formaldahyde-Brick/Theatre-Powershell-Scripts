f5b23c6567cddbd6ce80e1a0abca80eb309d48ca
Colin Evans <cybclinic@gmail.com>
2023-04-01 08:35:50 +0100

    Added autocreation of unique top folder name

--- New-MultiShow.ps1
+++ New-MultiShow.ps1
@@ -7,9 +7,28 @@ function New-MultiShow {
     )
     $d = Get-Date
     $justDate = $d.tostring("dd-MM-yyyy")
-    [string]$fullName = "$showName"+"-"+"$justDate"
+    [string]$fullName = "$showName"+"_"+"$justDate"
     If (Test-Path $fullName){
-    Write-Host "Folder exists, use a unique name"
+    Write-Host "Folder exists, creating a unique name"
+    $k = 1
+        [string]$extendName = "$ShowName"+"$k"+"_"+"$justDate"
+        while (Test-Path $extendName) {
+            $k++
+            [string]$extendName = "$ShowName"+"$k"+"_"+"$justDate"
+        }
+        New-Item -Name $extendName -ItemType Directory
+        Set-Location $extendName
+        [int]$noOfPlays = Read-Host "How many Plays?"
+        for ($i -eq 0; $i -lt $noOfPlays; $i++) {
+            $j = $i+1
+            $nameOfPlay = Read-Host "Name of Play"$j"?"
+            New-Item -Name $nameOfPlay -ItemType Directory
+            Set-Location $nameOfPlay
+            New-Item -Name Audio -ItemType Directory
+            New-Item -Name Images -ItemType Directory
+            New-Item -Name Video -ItemType Directory
+            Set-Location ..
+            }
     }
     Else {
         New-Item -Name $fullName -ItemType Directory
@@ -24,7 +43,7 @@ function New-MultiShow {
             New-Item -Name Images -ItemType Directory
             New-Item -Name Video -ItemType Directory
             Set-Location ..
-        }
-        Set-Location ..
+        }            
     }
+    Set-Location ..
 }
--- New-Show.ps1
+++ New-Show.ps1
@@ -7,9 +7,21 @@ function New-Show  {
     )
     $d = Get-Date
     $justDate = $d.tostring("dd-MM-yyyy")
-    [string]$fullName = "$showName"+"-"+"$justDate"
+    [string]$fullName = "$showName"+"_"+"$justDate"
     If (Test-Path $fullName){
-    Write-Host "Folder exists, use a unique name"
+        Write-Host "Folder exists, creating a unique name"
+        $i = 1
+        [string]$extendName = "$ShowName"+"$i"+"_"+"$justDate"
+        while (Test-Path $extendName) {
+            $i++
+            [string]$extendName = "$ShowName"+"$i"+"_"+"$justDate"
+        }
+        New-Item -Name $extendName -ItemType Directory
+        Set-Location $extendName
+        New-Item -Name Audio -ItemType Directory
+        New-Item -Name Images -ItemType Directory
+        New-Item -Name Video -ItemType Directory
+        Set-Location ..
     }
     Else {
         New-Item -Name $fullName -ItemType Directory
